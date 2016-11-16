d66848dd121d23dfbc980a133d7286dde96937a2
Jonathan Polonsky <jonny.polonsky@gmail.com>
2016-11-16 08:09:21 -0500

    no message

--- functions.R
+++ functions.R
@@ -4,144 +4,6 @@ WrangleData <- function(dept = NULL){
   
   sheetnames <- excel_sheets(list_path)
   
-  exclude_sheets <- 
-    c(
-      sheetnames[str_detect(sheetnames, 'Sheet') == TRUE],
-      sheetnames[str_detect(sheetnames, 'List') == TRUE],
-      sheetnames[str_detect(sheetnames, 'DS') == TRUE],
-      sheetnames[str_detect(sheetnames, 'Inst') == TRUE],
-      'Ouverture', 'A', 'Z', 'BTS', "Taux d'Attaque", 'Report_staging', 'Tableaux_Tests Cholera'
-    )
-  
-  list_commune <- sheetnames[!sheetnames %in% exclude_sheets]
-  
-  PrepareData <- function(dept = NULL, commune = 'Baraderes') {
-    
-    dat <- 
-      read_excel(path = list_path, sheet = commune, skip = 8) %>% 
-      setNames(make.names(names(.)))
-    
-    dat_names <- 
-      c('date', 'epiweek', 'cas_vus_<5', 'cas_vus_5+', 'cas_vus_tot', 'cas_hosp_<5', 'cas_hosp_5+', 'cas_hosp_tot', 'deces_inst_<5', 'deces_inst_5+', 'deces_inst_tot', 'deces_comm_<5', 'deces_comm_5+', 'deces_comm_tot', 'deces_tot_no_age', 'deces_tot')
-    
-    dat <-
-      dat[1:16] %>%
-      setNames(dat_names) %>%
-      mutate(
-        dept = dept,
-        commune = commune,
-        year = year(date),
-        month = month(date, label = TRUE),
-        week_year = paste(year(date), ifelse(nchar(isoweek(date)) == 1, paste0('0', isoweek(date)), isoweek(date)), sep = '-'),
-        epiweek_year = paste(year(date), ifelse(nchar(epiweek) == 1, paste0('0', epiweek), epiweek), sep = '-')
-      ) %>%
-      select(dept, commune, date, month, week_year, epiweek_year, everything())
-    
-    return(dat)
-    
-  }
-  
-  list_dat <- map(list_commune, PrepareData, dept = dept) %>% set_names(list_commune) %>% bind_rows
-  return(list_dat)
-  
-}
-
-
-AnalyseData <- function(data = NULL, variable = NULL){
-  
-  data %>%
-    bind_rows %>%
-    filter(date < today()) %>%
-    group_by(dept, commune, date, year, month, epiweek_year, epiweek) %>%
-    summarise_(n = lazyeval::interp(~sum(var), var = as.name(variable)))
-  
-}
-
-
-# plotting functions
-PlotHistYear <- function(data = NULL){
-  
-  ggplot(data) +
-    geom_bar(aes(x = month, y = n, fill = factor(year)), stat = 'identity') +
-    scale_fill_viridis(discrete = TRUE) +
-    facet_wrap(~year) +
-    ggthemes::theme_tufte()
-  
-}
-
-PlotHistCommune <- function(data = NULL){
-  
-  ggplot(filter(data, year >= 2015)) +
-    geom_bar(aes(x = epiweek_year, y = n, fill = commune), stat = 'identity') +
-    scale_fill_viridis(discrete = TRUE) +
-    facet_wrap(~commune) +
-    ggthemes::theme_tufte()
-  
-}
-
-# Post-hurricane plots
-PlotHistDayPostHurr <- function(data = NULL){
-  
-  ggplot(data) +
-    geom_bar(aes(x = date, y = n), stat = 'identity') +
-    # geom_bar(aes(x = date, y = n, fill = commune), stat = 'identity') +
-    # scale_fill_viridis(discrete = TRUE) +
-    geom_text(
-      data = data %>% group_by(date) %>% summarise(total = sum(n)), 
-      aes(x = date, y = total + max(total)/50, label = total), size = 1
-    ) +
-    labs(x = 'Date', y = '# cases') +
-    # ggthemes::theme_tufte() +
-    theme_bw() +
-    theme(
-      axis.title = element_text(size = 4), 
-      axis.text = element_text(size = 3),
-      axis.ticks = element_blank()
-      ) 
-  
-}
-
-PlotHistWeekPostHurr <- function(data = NULL){
-  
-  ggplot(data) +
-    geom_bar(aes(x = epiweek, y = n), stat = 'identity') +
-    # geom_bar(aes(x = epiweek, y = n, fill = commune), stat = 'identity') +
-    # scale_fill_viridis(discrete = TRUE) +
-    geom_text(
-      data = data %>% group_by(epiweek) %>% summarise(total = sum(n)), 
-      aes(x = epiweek, y = total + max(total)/50, label = total), size = 1
-    ) +
-    labs(x = 'Epiweek 2016', y = '# cases') +
-    ggthemes::theme_tufte()
-  
-}
-
-PlotHistWeekCommunePostHurr <- function(data = NULL){
-  
-  ggplot(data) +
-    geom_bar(aes(x = epiweek, y = n), stat = 'identity') +
-    # geom_bar(aes(x = epiweek, y = n, fill = commune), stat = 'identity') +
-    # scale_fill_viridis(discrete = TRUE) +
-    geom_text(
-      data = data %>% group_by(commune, epiweek) %>% summarise(total = sum(n)), 
-      aes(x = epiweek, y = total + max(total)/25, label = total), size = 1
-    ) +
-    facet_wrap(~commune) +
-    labs(x = 'Epiweek 2016', y = '# cases') +
-    theme(legend.position = 'none', axis.line = element_line(color = "black")) +
-    ggthemes::theme_tufte() 
-    # ggthemes::theme_tufte(base_family = 'BemboStd')
-  
-}
-
-
-# Alternative source of data
-WrangleDataCTC <- function(dept = NULL){
-  
-  list_path <- list_db_paths[str_detect(list_db_paths, dept) == TRUE]
-  
-  sheetnames <- excel_sheets(list_path)
-  
   sheet <- sheetnames[str_detect(sheetnames, 'Saisie') == TRUE]
   
   dat_raw <- 
@@ -194,7 +56,7 @@ WrangleDataCTC <- function(dept = NULL){
 }
 
 
-PlotHistDayPostHurrCTC <- function(data = NULL){
+PlotHistDay <- function(data = NULL){
   
   ggplot(
     data %>% filter(key %in% 'cas_vus')
@@ -221,7 +83,46 @@ PlotHistDayPostHurrCTC <- function(data = NULL){
   
 }
 
-PlotHistWeekPostHurrCTC <- function(data = NULL){
+
+PlotHistDayInst <- function(data = NULL){
+  
+  dat <- 
+    # data %>%
+    list_dat[[1]] %>%
+    filter(key == 'cas_vus') %>% 
+    ungroup() %>% 
+    mutate(inst = ifelse(inst == 'UTC', paste(inst, commune), inst)) %>% 
+    group_by(inst, date, key) %>%
+    summarise(n = sum(n))
+  
+  exclude <- 
+    dat %>% 
+    group_by(inst) %>% 
+    summarise(n = sum(n)) %>% 
+    filter(n == 0) %>% 
+    c
+  
+  ggplot(
+    dat %>% filter(!inst %in% exclude$inst)
+  ) +
+    geom_bar(aes(x = date, y = n), stat = 'identity') +
+    facet_wrap(~ inst) +
+    labs(x = 'Date', y = '# cases') +
+    ggthemes::theme_tufte(base_family = 'Palatino') +
+    theme(
+      legend.position = 'none', 
+      axis.line = element_line(color = "black"),
+      axis.title = element_text(size = 6), 
+      axis.text.x = element_text(size = 5, angle = 45),
+      axis.text.y = element_text(size = 5),
+      strip.text = element_text(size = 5),
+      axis.ticks = element_blank()
+    )
+  
+}
+
+
+PlotHistWeek <- function(data = NULL){
   
   tmp <- 
     data %>% 
@@ -242,42 +143,27 @@ PlotHistWeekPostHurrCTC <- function(data = NULL){
   
 }
 
-PlotHistWeekInstPostHurrCTC <- function(data = NULL){
+
+PlotHistWeekCommune <- function(data = NULL){
   
-  tmp <- 
-    data %>% 
+  dat <- 
+    data %>%
+    # list_dat_post_hurr[[1]] %>% 
+    filter(key == 'cas_vus') %>% 
     mutate(epiweek = epitools::as.week(date)[['week']]) %>%
-    group_by(inst, commune, epiweek, key) %>%
+    group_by(commune, epiweek) %>%
     summarise(n = sum(n))
   
-  ggplot(tmp) +
-    geom_bar(aes(x = epiweek, y = n), stat = 'identity') +
-    facet_wrap(~inst) +
-    labs(x = 'Epiweek 2016', y = '# cases') +
-    theme(legend.position = 'none', axis.line = element_line(color = "black")) +
-    ggthemes::theme_tufte() +
-    # theme_bw() +
-    theme(
-      legend.position = 'none', 
-      axis.line = element_line(color = "black"),
-      axis.title = element_text(size = 6), 
-      axis.text = element_text(size = 5),
-      strip.text = element_text(size = 5),
-      axis.ticks = element_blank()
-    )
+  exclude <- 
+    dat %>% 
+    group_by(commune) %>% 
+    summarise(n = sum(n)) %>% 
+    filter(n == 0) %>% 
+    c
   
-}
-
-
-PlotHistWeekCommunePostHurrCTC <- function(data = NULL){
-    
-  tmp <- 
-    data %>% 
-    mutate(epiweek = epitools::as.week(date)[['week']]) %>%
-    group_by(inst, commune, epiweek, key) %>%
-    summarise(n = sum(n))
-  
-  ggplot(tmp) +
+  ggplot(
+    dat %>% filter(!commune %in% exclude$commune)
+  ) +
     geom_bar(aes(x = epiweek, y = n), stat = 'identity') +
     facet_wrap(~ commune) +
     labs(x = 'Epiweek 2016', y = '# cases') +
@@ -297,8 +183,50 @@ PlotHistWeekCommunePostHurrCTC <- function(data = NULL){
   
 }
 
+
+PlotHistWeekInst <- function(data = NULL){
+  
+  dat <- 
+    data %>%
+    # list_dat_post_hurr[[1]] %>%
+    filter(key == 'cas_vus') %>% 
+    ungroup() %>% 
+    mutate(inst = ifelse(inst == 'UTC', paste(inst, commune), inst)) %>% 
+    mutate(epiweek = epitools::as.week(date)[['week']]) %>%
+    group_by(inst, epiweek, key) %>%
+    summarise(n = sum(n))
+  
+  exclude <- 
+    dat %>% 
+    group_by(inst) %>% 
+    summarise(n = sum(n)) %>% 
+    filter(n == 0) %>% 
+    c
+  
+  ggplot(
+    dat %>% filter(!inst %in% exclude$inst)
+  ) +
+    geom_bar(aes(x = epiweek, y = n), stat = 'identity') +
+    facet_wrap(~inst) +
+    labs(x = 'Epiweek 2016', y = '# cases') +
+    theme(legend.position = 'none', axis.line = element_line(color = "black")) +
+    ggthemes::theme_tufte() +
+    # theme_bw() +
+    theme(
+      legend.position = 'none', 
+      axis.line = element_line(color = "black"),
+      axis.title = element_text(size = 6), 
+      axis.text = element_text(size = 5),
+      strip.text = element_text(size = 5),
+      axis.ticks = element_blank()
+    )
+  
+}
+
+
+
 # Table
-MakeTablePostHurrCTC <- function(data){
+MakeTable <- function(data){
   
   tmp <- 
     data %>%
--- rough.R
+++ rough.R
@@ -13,31 +13,33 @@ library(extrafont)
 
 source('functions.R')
 
-# list data sheets to be read in (all communes per dept)
-list_db_paths <- dir('data', full.names = TRUE)
-list_depts <- c('DSGA', 'DSS')
-
-list_dat <- map(list_depts[1], WrangleData)
-
-list_variables <- c('cas_vus_tot', 'cas_hosp_tot', 'deces_inst_tot', 'deces_comm_tot', 'deces_tot')
-
-list_dat <- map(list_variables, AnalyseData, data = list_dat) %>% set_names(list_variables)
-
-list_hist_year <- map(list_dat, PlotHistYear)
-list_hist_commune <- map(list_dat, PlotHistCommune)
-
-# since the hurricane
-list_dat_post_hurr <- 
-  map(list_dat, function(data) data %>% filter(year == '2016', epiweek >= 40) %>% mutate(epiweek = factor(epiweek)))
-
-# by day
-list_hist_day_post_hurr <- map(list_dat_post_hurr, PlotHistDayPostHurr)
-
-# by week
-list_hist_week_post_hurr <- map(list_dat_post_hurr, PlotHistWeekPostHurr)
-
-# by week & commune
-list_hist_week_commune_post_hurr <- map(list_dat_post_hurr, PlotHistWeekCommunePostHurr)
+# # list data sheets to be read in (all communes per dept)
+# source('functions_orig.R')
+# 
+# list_db_paths <- dir('data', full.names = TRUE)
+# list_depts <- c('DSGA', 'DSS')
+# 
+# list_dat <- map(list_depts[1], WrangleData)
+# 
+# list_variables <- c('cas_vus_tot', 'cas_hosp_tot', 'deces_inst_tot', 'deces_comm_tot', 'deces_tot')
+# 
+# list_dat <- map(list_variables, AnalyseData, data = list_dat) %>% set_names(list_variables)
+# 
+# list_hist_year <- map(list_dat, PlotHistYear)
+# list_hist_commune <- map(list_dat, PlotHistCommune)
+# 
+# # since the hurricane
+# list_dat <- 
+#   map(list_dat, function(data) data %>% filter(year == '2016', epiweek >= 40) %>% mutate(epiweek = factor(epiweek)))
+# 
+# # by day
+# list_hist_day <- map(list_dat, PlotHistDay)
+# 
+# # by week
+# list_hist_week <- map(list_dat, PlotHistWeek)
+# 
+# # by week & commune
+# list_hist_week_commune <- map(list_dat, PlotHistWeekCommune)
 
 
 ## read in different page (CTC - 'Saisie Inst')
@@ -46,21 +48,24 @@ list_depts <-
   list_db_paths[str_detect(list_db_paths, 'Surveillance Cholera_2015_2016')] %>% 
   str_replace_all(c('data/' = '', '[-_]Surveillance Cholera.*' = ''))
 
-list_dat_post_hurr <- map(list_depts, WrangleDataCTC) %>% set_names(list_depts)
+list_dat <- map(list_depts, WrangleData) %>% set_names(list_depts)
 
 # plot by day
-list_hist_post_hurr_day <- map(list_dat_post_hurr, PlotHistDayPostHurrCTC) %>% set_names(list_depts)
+list_hist_day <- map(list_dat, PlotHistDay) %>% set_names(list_depts)
+
+# by day & inst
+list_hist_day_inst <- map(list_dat, PlotHistDayInst) %>% set_names(list_depts)
 
 # plot by epiweek
-list_hist_post_hurr_week <- map(list_dat_post_hurr, PlotHistWeekPostHurrCTC) %>% set_names(list_depts)
+list_hist_week <- map(list_dat, PlotHistWeek) %>% set_names(list_depts)
 
-# by week & inst
-list_hist_post_hurr_week_inst <- map(list_dat_post_hurr, PlotHistWeekCommunePostHurrCTC)
-PlotHistWeekCommunePostHurrCTC(list_dat_post_hurr[[1]])
+# by epiweek & inst
+list_hist_week_inst <- map(list_dat, PlotHistWeekInst) %>% set_names(list_depts)
+
 
 # tables
-list_table_post_hurr_CTC <- map(list_dat_post_hurr, MakeTablePostHurrCTC)  
-
+list_table <- map(list_dat, MakeTable) %>% set_names(list_depts)
 
 
 # maps
+map_haiti <- rgdal::readOGR("map/HTI_adm3.shp", "HTI_adm3")
